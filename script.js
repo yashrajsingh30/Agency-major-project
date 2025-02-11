043d423cf39f92be8526f83eb145c732739f4115
Yash Raj Singh <singhyashraj3004@gmail.com>
2025-02-11 22:52:24 +0530

    Minor tweaking

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-gsap.registerPlugin(ScrollTrigger);
+function locomotiveScroll() {
+  gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
 
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
-
-
-
-
-
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
 
 function loadinganimation() {
   var tl = gsap.timeline();
@@ -110,42 +114,41 @@ function cursorAnimation() {
 }
 
 function contentHover() {
-    var hover1 = document.querySelector("#hover1");
-    var hover2 = document.querySelector("#hover2");
-    hover1.addEventListener("mouseenter", function () {
-      hover1.style.webkitTextStroke = "1px white";
-  
-      hover1.style.color = "transparent";
-      hover1.style.textDecoration = "none";
-  
-      hover1.style.transition = "cubic-bezier(0.19, 1, 0.22, 1) 0.4s";
-    });
-  
-    hover1.addEventListener("mouseleave", function () {
-      hover1.style.webkitTextStroke = "0px white";
-  
-      hover1.style.color = "#fff";
-      hover1.style.textDecoration = "underline";
-      hover1.style.transition = "cubic-bezier(0.19, 1, 0.22, 1) 0.4s";
-    });
-  
-    hover2.addEventListener("mouseenter", function () {
-      hover2.style.webkitTextStroke = "1px white";
-  
-      hover2.style.color = "transparent";
-      hover2.style.textDecoration = "none";
-      hover2.style.transition = "cubic-bezier(0.19, 1, 0.22, 1) 0.4s";
-    });
-  
-    hover2.addEventListener("mouseleave", function () {
-      hover2.style.webkitTextStroke = "0px white";
-      hover2.style.color = "#fff";
-      hover2.style.textDecoration = "underline";
-      hover2.style.transition = "cubic-bezier(0.19, 1, 0.22, 1) 0.4s";
-    });
-  }
+  var hover1 = document.querySelector("#hover1");
+  var hover2 = document.querySelector("#hover2");
+  hover1.addEventListener("mouseenter", function () {
+    hover1.style.webkitTextStroke = "1px white";
+
+    hover1.style.color = "transparent";
+    hover1.style.textDecoration = "none";
+
+    hover1.style.transition = "cubic-bezier(0.19, 1, 0.22, 1) 0.4s";
+  });
+
+  hover1.addEventListener("mouseleave", function () {
+    hover1.style.webkitTextStroke = "0px white";
+
+    hover1.style.color = "#fff";
+    hover1.style.textDecoration = "underline";
+    hover1.style.transition = "cubic-bezier(0.19, 1, 0.22, 1) 0.4s";
+  });
+
+  hover2.addEventListener("mouseenter", function () {
+    hover2.style.webkitTextStroke = "1px white";
+
+    hover2.style.color = "transparent";
+    hover2.style.textDecoration = "none";
+    hover2.style.transition = "cubic-bezier(0.19, 1, 0.22, 1) 0.4s";
+  });
+
+  hover2.addEventListener("mouseleave", function () {
+    hover2.style.webkitTextStroke = "0px white";
+    hover2.style.color = "#fff";
+    hover2.style.textDecoration = "underline";
+    hover2.style.transition = "cubic-bezier(0.19, 1, 0.22, 1) 0.4s";
+  });
+}
 
 loadinganimation();
 cursorAnimation();
 contentHover();
-
